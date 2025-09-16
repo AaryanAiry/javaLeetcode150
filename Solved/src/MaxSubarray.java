dff6e4e81bcf71830a6edc533934bf1bcc079fbe
AaryanAiry <161134110+AaryanAiry@users.noreply.github.com>
2025-09-17 01:33:30 +0530

    Create MaxSubarray.java

---
+++ Solved/src/MaxSubarray.java
@@ -0,0 +1,15 @@
+//Kadane's Algorithm: 
+// localMax = max(arr[i],localMax+arr[i])
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int maxSum = nums[0];
+        int localMax = nums[0];
+        for(int i=1;i<n;i++){
+            localMax = Math.max(nums[i],localMax+nums[i]);
+            maxSum = Math.max(maxSum,localMax);
+        }
+        return maxSum;
+    }
+}
